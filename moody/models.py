eda864be7679d559f685334b70e0ab9b1f234d23
divadluca14 <divadluca14@gmail.com>
2021-10-28 10:37:16 +0300

    Create UserLocation model

--- moody/models.py
+++ moody/models.py
@@ -1,15 +1,24 @@
-from django.db import models
 from django.contrib.gis.db import models
 
 
 class Users(models.Model):
     name = models.CharField(max_length=255, blank=True, null=True)
-    addr_home = models.PolygonField()
-    addr_office = models.PolygonField()
+    # addr_home = models.PolygonField()
+    # addr_office = models.PolygonField()
 
     class Meta:
         managed = False
         db_table = 'users'
+
+
+class UserLocations(models.Model):
+    name = models.CharField(max_length=255, blank=True, null=True)
+    mpoly = models.MultiPolygonField()
+
+    user_id = models.ForeignKey(Users, related_name='locations', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
 
 class Moods(models.Model):
